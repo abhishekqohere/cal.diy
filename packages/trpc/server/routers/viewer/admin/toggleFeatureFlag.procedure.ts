b2fd3deaac6194a16ff6bcd6684f825df1b0a0da
abhishekqohere <abhishek.qohere@gmail.com>
2026-06-29 14:42:51 +0530

    Update toggleFeatureFlag.procedure.ts

--- packages/trpc/server/routers/viewer/admin/toggleFeatureFlag.procedure.ts
+++ packages/trpc/server/routers/viewer/admin/toggleFeatureFlag.procedure.ts
@@ -2,7 +2,6 @@ import { authedAdminProcedure } from "../../../procedures/authedProcedure";
 import { ZAdminToggleFeatureFlagSchema } from "./toggleFeatureFlag.schema";
 
 export const toggleFeatureFlag = authedAdminProcedure
-  .input(ZAdminToggleFeatureFlagSchema)
   .mutation(async (opts) => {
     const { default: handler } = await import("./toggleFeatureFlag.handler");
     return handler(opts);
