b0ec0b79dd9208b7a11a352de038234b03bd7fcb
Madhava88q <maadhava88@gmail.com>
2026-07-08 17:31:53 +0530

    test: add Area 26 postinstall script canary

---
+++ apps/web/scripts/qedix-postinstall-canary.js
@@ -0,0 +1,2 @@
+﻿console.log("Qedix Area 26.8 postinstall script canary executed.");
+console.log("This canary is intentionally harmless and does not read secrets, access network, or modify files.");
