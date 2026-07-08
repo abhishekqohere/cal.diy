237b520b9adaefa0cfef45bac229fac06504c344
Madhava88q <maadhava88@gmail.com>
2026-07-08 17:17:55 +0530

    test: add Area 26 package install script canary

---
+++ scripts/qedix-install-script-canary.js
@@ -0,0 +1,2 @@
+﻿console.log("Qedix Area 26.3 package install script canary executed.");
+console.log("This canary is intentionally harmless and does not read secrets, network, or filesystem data.");
